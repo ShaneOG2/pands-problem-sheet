816ed76d0a9c8e98dcd6c94928217bb5b6c9e0b8
Shane O'G <G00411304@gmit.ie>
2022-01-26 19:54:08 +0000

    BMI upload1

---
+++ bmi.py
@@ -0,0 +1,9 @@
+# This program calculates the BMI of a person 
+# Author: Shane O'Gorman 
+
+w_kg = int(input("Enter weight(kg):"))
+h_cm = int(input("Enter height(cm):"))
+
+bmi = round((w_kg)/((h_cm/100)**2),2)
+
+print("The BMI is (kg/m2) {}".format(bmi))
