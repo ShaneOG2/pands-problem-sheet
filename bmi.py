649621a2da219e3e94ef56f52b3e109d5dfd492e
Shane O'G <G00411304@gmit.ie>
2022-03-21 19:15:22 +0000

    ReadMe test

--- bmi.py
+++ bmi.py
@@ -8,4 +8,6 @@ bmi = round((w_kg)/((h_cm/100)**2),2)
 
 print("The BMI is (kg/m2) {}".format(bmi))
 
-#Reference: round function https://www.w3schools.com/python/ref_func_round.asp
+#Reference: 
+#BMI formula https://www.cdc.gov/nccdphp/dnpao/growthcharts/training/bmiage/page5_1.html#:~:text=With%20the%20metric%20system%2C%20the,by%2010%2C000%2C%20can%20be%20used.
+#round function https://www.w3schools.com/python/ref_func_round.asp
